107a3bb2c52e13c23fe8dde8cd70b738643beefc
Henri Niva <henri.niva@aalto.fi>
2015-12-06 12:57:53 +0200

    Day 6 - Part 2

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -34,5 +34,34 @@ def main():
 
     print sum(map(sum, grid))
 
+    #Part 2
+
+    grid = [[0 for x in xrange(1000)] for y in xrange(1000)]
+
+    for line in data:
+        firstCoord = re.search("\\d", line)
+        topleft = line[firstCoord.start():]
+        topleft = topleft.split(" ")[0].split(",")
+        topleft = [int(x) for x in topleft]
+
+        secondCoord = re.search("through", line)
+        bottomright = line[secondCoord.start():].split(" ")[1].split(",")
+        bottomright = [int(x) for x in bottomright]
+
+        if "turn on" in line:
+            for x in xrange(topleft[0], bottomright[0] + 1):
+                for y in xrange(topleft[1], bottomright[1] + 1):
+                    grid[x][y] += 1
+        elif "turn off" in line:
+            for x in xrange(topleft[0], bottomright[0] + 1):
+                for y in xrange(topleft[1], bottomright[1] + 1):
+                    grid[x][y] = max(0, grid[x][y] - 1)
+        elif "toggle" in line:
+            for x in xrange(topleft[0], bottomright[0] + 1):
+                for y in xrange(topleft[1], bottomright[1] + 1):
+                    grid[x][y] += 2
+
+    print sum(map(sum, grid))
+
 if __name__ == '__main__':
     main()
