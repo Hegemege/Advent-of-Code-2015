ad9456942e849b202de86807e476a1bc85ea0bf8
Henri Niva <henri.niva@aalto.fi>
2015-12-09 17:03:56 +0200

    Day 9 - Part 1 and 2

---
+++ Day 9/day9.py
@@ -0,0 +1,62 @@
+import copy
+
+def main():
+    f = open("in.txt", "r")
+    data = f.readlines()
+    f.close()
+
+    data = [x.replace("\n", "") for x in data]
+
+    cities = { }
+
+    for line in data:
+        dims = line.split(" = ")
+        dist = int(dims[1])
+        fromCity = dims[0].split(" to ")[0]
+        toCity = dims[0].split(" to ")[1]
+
+        if fromCity not in cities:
+            cities[fromCity] = {}
+        cities[fromCity][toCity] = dist
+
+        if toCity not in cities:
+            cities[toCity] = {}
+        cities[toCity][fromCity] = dist
+
+    for key, value in cities.iteritems(): #start from any city
+        shortest = [99999]
+        traverseFrom(cities, key, [key], [], shortest)
+        print key, shortest
+
+    # Part 2
+
+    print "---------"
+
+    for key, value in cities.iteritems():
+        longest = [0]
+        traverseFromLong(cities, key, [key], [], longest)
+        print key, longest
+
+
+def traverseFrom(cities, cityname, visited, s, shortest):
+    for key, value in cities[cityname].iteritems():
+        if key in visited:
+            continue
+        traverseFrom(cities, key, visited + [key], s + [value], shortest)
+
+    if len(visited) == len(cities):
+        if sum(s) < shortest[0]:
+            shortest[0] = sum(s)
+
+def traverseFromLong(cities, cityname, visited, s, longest):
+    for key, value in cities[cityname].iteritems():
+        if key in visited:
+            continue
+        traverseFromLong(cities, key, visited + [key], s + [value], longest)
+
+    if len(visited) == len(cities):
+        if sum(s) > longest[0]:
+            longest[0] = sum(s)
+
+if __name__ == '__main__':
+    main()
