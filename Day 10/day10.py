b7ca83e31c6cab77caf5d7d4a7cb92898d0837a8
Henri Niva <henri.niva@aalto.fi>
2015-12-10 20:03:40 +0200

    Day 10 - Part 2

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -1,12 +1,12 @@
 from itertools import groupby
 
 def main():
-    data = "3113322113"
+    for part in [40, 50]:
+        data = "3113322113"
+        for i in xrange(part):
+            data = ''.join(str(len(list(value))) + key for key, value in groupby(data))
 
-    for i in xrange(40):
-        data = ''.join(str(len(list(value))) + key for key, value in groupby(data))
-
-    print len(data)
+        print len(data)
 
 
 if __name__ == '__main__':
