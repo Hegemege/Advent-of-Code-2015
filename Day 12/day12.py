1032a5b5f3622dcd36d645dbae29ae6e60fcec80
Henri Niva <henri.niva@aalto.fi>
2015-12-12 13:31:41 +0200

    Day 12 - Part 2

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 def main():
     f = open("in.txt", "r")
@@ -10,8 +11,31 @@ def main():
     p1 = re.compile("-?[0-9]+")
     print sum(map(int, p1.findall(data)))
 
+    jsonobj = json.loads(data)
+    print walk(jsonobj)
 
 
+def walk(data):
+    try:
+        if type(data) is list:
+            totalsum = 0
+            for value in data:
+                totalsum += walk(value)
+            return totalsum
+            #return sum(map(int, filter(lambda x: x == True, [x.isdigit() for x in data])))
+        elif type(data) is dict:
+            for key, value in data.iteritems():
+                if value == "red":
+                    return 0
+            totalsum = 0
+            for key, value in data.iteritems():
+                totalsum += walk(value)
+            return totalsum
+        elif type(data) is int:
+            return data
+    except ValueError as e:
+        pass
+    return 0
 
 
 if __name__ == '__main__':
